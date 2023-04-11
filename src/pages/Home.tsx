7ff9e45807a9e1e969137baec7f6caadf0206cf3
YamilH <yamilhamui@gmail.com>
2023-04-11 16:38:14 -0300

    pausa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
+
+import homevideo from '/media/cafelindo.mp4'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+  <h1 className='h1'>Despensa San Juan</h1>
+
+  <video src={homevideo} autoPlay loop muted className="object-fit-cover w-100 bg-video"
+   />
+      <div className="container position-absolute">
+        <h1 className="text-light">Depensa San Juan</h1>
+        <p className="text-light">Welcome to my website</p>
+      </div>
+  </>
+  )
 }
